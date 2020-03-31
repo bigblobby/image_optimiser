d42159a6d374eaa70f5f9c4dd3544cdc88c947ba
Dempster <tom_dempster@hotmail.co.uk>
2020-03-31 21:47:41 +0100

    added api class, frontend

---
+++ client/src/js/api.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+class Api {
+    cache = {};
+
+    get(url){
+        return new Promise((resolve, reject) => {
+            axios.get(url)
+                .then(response => {
+                    resolve(response.data);
+                }).catch(err => {
+                    console.error(err);
+                    reject(err.response.data);
+                });
+        });
+    }
+
+    post(url, params){
+        return new Promise((resolve, reject) => {
+            axios.post(url, params)
+                .then(response => {
+                    resolve(response.data);
+                }).catch(err => {
+                    console.error(err);
+                    reject(err.response.data);
+                });
+        });
+    }
+
+    uploadImage(params = {}){
+        const url = '/api/image/upload';
+        return this.post(url, params);
+    }
+}
+
+export default new Api();
--- client/src/js/pages/Homepage.js
+++ client/src/js/pages/Homepage.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import Api from '../api';
 
 class Homepage extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount(){
+        Api.uploadImage()
+            .then(result => {
+                console.log(result);
+            }).catch(err => {
+                console.log(err);
+        });
+    }
+
     render() {
         return (
             <div>
