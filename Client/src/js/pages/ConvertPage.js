a61f252c03d9b20d476f4c7df33776490f39d8e1
Dempster <tom_dempster@hotmail.co.uk>
2020-04-17 22:54:26 +0100

    start convert page

--- Client/src/js/pages/ConvertPage.js
+++ Client/src/js/pages/ConvertPage.js
@@ -1,18 +1,59 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import DragAndDrop from "../components/DragAndDrop";
+import { resetOptimiser, updateDisplayAndUploadFiles } from "../actions/imageOptimiserActions";
+import Helpers from "../helpers";
 
 class ConvertPage extends React.Component {
     constructor(props) {
         super(props);
     }
 
+    componentDidMount(){
+        this.props.resetOptimiser();
+    }
+
+    handleFiles = async (files) => {
+        const images = await Helpers.fileListBase64(files);
+        this.props.updateDisplayAndUploadFiles(images);
+    };
+
     render() {
         return (
-            <div className="homepage">
-                <p>Convert page</p>
+            <div className="convert-page">
+                <div className="container-fluid max-width">
+                    <h1 className="text-center">Convert images</h1>
+                    <p className="heading-caption text-center mb-5">Blah blah blah</p>
+                    <div className="convert-page--inner">
+                        <div className="drag-and-drop--container">
+                            <DragAndDrop
+                                fileLimit={12}
+                                handleFiles={this.handleFiles}
+                                text={"Drag and drop your image or click here"}
+                                helpText={"(1 image only)"}
+                                acceptedFileTypes={['image/png', 'image/jpeg', 'image/svg+xml']}
+                            />
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
 }
 
-export default connect(null, null)(ConvertPage);
+const mapStateToProps = ({imageOptimiser}) => {
+    const {images, error} = imageOptimiser;
+    return {
+        images,
+        error
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        updateDisplayAndUploadFiles: (displayFiles, uploadFiles) => dispatch(updateDisplayAndUploadFiles(displayFiles, uploadFiles)),
+        resetOptimiser: () => dispatch(resetOptimiser())
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ConvertPage);
