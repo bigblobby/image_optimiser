951751e66d1db4d24f1e8483f3442ce38ac558dc
Dempster <tom_dempster@hotmail.co.uk>
2020-05-10 17:17:20 +0100

    correct path

--- backend/Router/upload.router.js
+++ backend/Router/upload.router.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const controller = require('../Controller/upload.controller');
-const upload = require('../config/multer').upload;
+const upload = require('../Config/multer').upload;
 
 router.post('/single', controller.uploadSingle);
 router.post('/multiple', upload.array('images'), controller.uploadMultiple);
