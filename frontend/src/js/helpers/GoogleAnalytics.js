0a0108a41e8b2078589c6fca656d5af5a69381ab
Dempster <tom_dempster@hotmail.co.uk>
2020-05-19 21:46:02 +0100

    Fix GA script tags being loaded twice

--- frontend/src/js/helpers/GoogleAnalytics.js
+++ frontend/src/js/helpers/GoogleAnalytics.js
@@ -1,19 +1,19 @@
-import ReactGA from 'react-ga';
-
-class GoogleAnalytics {
-    init(){
-        ReactGA.initialize('UA-166789742-1');
-    }
-
-    set(options){
-        ReactGA.set(options);
-    }
-
-    pageview(path){
-        ReactGA.pageview(path);
-    }
-}
-
-const ga = new GoogleAnalytics();
-ga.init();
-export default ga;
+// import ReactGA from 'react-ga';
+//
+// class GoogleAnalytics {
+//     init(){
+//         ReactGA.initialize('UA-166789742-1');
+//     }
+//
+//     set(options){
+//         ReactGA.set(options);
+//     }
+//
+//     pageview(path){
+//         ReactGA.pageview(path);
+//     }
+// }
+//
+// const ga = new GoogleAnalytics();
+// //ga.init();
+// export default ga;
